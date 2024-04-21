a61254300d3b4907927ef52063c899d61c53ead6
Anirban Kapil <anirbankapil@gmail.com>
2024-04-21 11:09:12 +0530

    hooks practise

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,14 +1,27 @@
 
-import React from 'react';
+import { useState } from "react"
 
 
 function App() {
+
+  let [counter,setCounter] = useState(0);
   
+function increment () {
+     if(counter == 20) return; 
+     setCounter(counter + 1);
+}
+
+function decrement () {
+  if(counter == 0 ) return
+  setCounter(counter -1);
+}
 
   return (
     
       <div> 
-       <h1>Hello</h1>
+       <h1>{counter}</h1>
+       <button onClick={increment}>Increase</button>
+       <button onClick={decrement}>Decrease</button>
       </div>
   )
 }
