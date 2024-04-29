c8ab26884863d290e0400395f579594c982235a4
Anirban Kapil <anirbankapil@gmail.com>
2024-04-29 21:43:54 +0530

    prototype

--- currencyConvertorProject/src/components/InputBox.jsx
+++ currencyConvertorProject/src/components/InputBox.jsx
@@ -1,5 +1,5 @@
 
-
+import { useId } from "react";
 
 
 
@@ -9,22 +9,25 @@ function InputBox({
     onAmountChange,
     onCurrencyChange,
     currencyOptions = [],
-    selectCurrency = "usd",
+    selectCurrency = "",
     amountDisable = false,
     currencyDisable = false,
 }
     
+   
+
 ) {
    
+    const amountInputId = useId();
 
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex `}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
