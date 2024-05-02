b207d95c8f3777cef72e4d758edcdfbe6cf88cee
Anirban Kapil <anirbankapil@gmail.com>
2024-05-02 22:58:07 +0530

    half way

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+
 
 
 function App() {
 
 
   return (
-    <>
-     <h1 className='bg-slate-400 shadow-lg text-center text-stone-50'>Hello Router</h1>
-    </>
+    <div>
+   
+    </div>
   )
 }
 
