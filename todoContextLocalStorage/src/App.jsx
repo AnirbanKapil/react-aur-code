e9c8e0fce17b0c1e2f92b47ce384d77f5ed4fdd9
Anirban Kapil <anirbankapil@gmail.com>
2024-05-07 00:18:38 +0530

    basic context functionality

--- todoContextLocalStorage/src/App.jsx
+++ todoContextLocalStorage/src/App.jsx
@@ -1,12 +1,41 @@
+import { useState } from "react"
+import { ToDoProvider } from "./contexts";
 
 
 function App() {
-  
+
+  const [toDo,setToDo] = useState([]);
+
+  const addToDo = (todo) => {
+    setToDo((preV)=> [{id: Date.now(),...todo},...preV])
+  };
+
+  const updateToDo = (id,todo) => {
+    setToDo((preV) => preV.map((preToDo)=> preToDo.id === id ? todo : preToDo))
+  };
+
+  const deleteToDo = (id) => {
+     setToDo((preV)=> preV.filter((todo)=> todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+      setToDo((preV)=> preV.map((todo)=> todo.id == id ? {...todo,completed : !todo.completed} : todo))
+  }
 
   return (
-    <>
-      <h1 className="bg-slate-400 shadow-lg text-white m-5 p-4 text-center font-extrabold">To-Do-App</h1>
-    </>
+    <ToDoProvider value={{todos,addToDo,updateToDo,deleteToDo,toggleComplete}}>
+    <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
+  </ToDoProvider>
   )
 }
 
