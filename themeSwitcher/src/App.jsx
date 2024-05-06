8732cda8a4a47478385afd50e082c99e2e92a3e9
Anirban Kapil <anirbankapil@gmail.com>
2024-05-06 13:41:38 +0530

    final product

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import { ThemeProvider } from "./contexts/theme"
+import ThemeBtn from "./components/ThemeButton";
+import Card from "./components/Card";
 
 
 function App() {
@@ -25,11 +27,11 @@ function App() {
 <div className="flex flex-wrap min-h-screen items-center">
       <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        
+              <ThemeBtn />          
            </div>
 
            <div className="w-full max-w-sm mx-auto">
-                       
+                <Card />       
         </div>
       </div>
 </div>
