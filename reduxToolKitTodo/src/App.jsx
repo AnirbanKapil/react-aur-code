57f915e5778240e9d15503fcb8f2b9c2e1a40ed8
Anirban Kapil <anirbankapil@gmail.com>
2024-05-09 23:29:14 +0530

    prototype

--- reduxToolKitTodo/src/App.jsx
+++ reduxToolKitTodo/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
   return (
     <>
       <h1>Redux-Tool-Kit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
--- reduxToolKitTodo/src/components/Todos.jsx
+++ reduxToolKitTodo/src/components/Todos.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import { useSelector,useDispatch } from 'react-redux'
+
+import { removeTodo } from '../features/todo/todoSlice'
+
+
 function Todos() {
+  
+  const todos = useSelector(state => state.todos);
+  const dispatch = useDispatch();  
+
   return (
-    <div>Todos</div>
+    <div>
+        <div>Todos</div>
+        {todos.map((todo) => (
+        <li key={todo.id}>
+            {todo.text}
+            <button onClick={()=> dispatch(removeTodo(todo.id))}>❌</button>
+            </li>))}
+    </div>
   )
 }
 
