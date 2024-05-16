39e5eb583837caf82dc5e3d4adfab5854cee8815
Anirban Kapil <anirbankapil@gmail.com>
2024-05-16 17:12:45 +0530

    auth service setup done

--- megaBlog/src/appwrite/auth.js
+++ megaBlog/src/appwrite/auth.js
@@ -20,6 +20,7 @@ export class AuthService{
            const userAccount = await this.account.create(ID.unique(),email,password,name);
            if(userAccount){
             //   call another method
+            return this.login({email,password})
            }else{
             return userAccount;
            } 
@@ -30,9 +31,24 @@ export class AuthService{
     
     async login ({email,password}){
         try {
-            
+            return await this.account.createEmailSession(email,password)
         } catch (error) {
             throw error
+        }
+    }
+    async getCurrentUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("Appwrite service get current user error -", error )
+        }
+        return null;
+    }
+    async logout(){
+        try {
+           await this.account.deleteSessions(); 
+        } catch (error) {
+            console.log("Appwrite service logout error -" , error)
         }
     }
 }
