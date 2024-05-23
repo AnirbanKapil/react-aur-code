510562a445c3ebb3a796b8deba10c240a9deac23
Anirban Kapil <anirbankapil@gmail.com>
2024-05-23 21:39:36 +0530

    complete version

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     if(numAllowed) str += "0123456789";
     if(charAllowed) str += "!@#$%^&*~";
       
-    for(let i = 0 ; i <= length ; i++){
+    for(let i = 1 ; i <= length ; i++){
       let char = Math.floor(Math.random()*str.length + 1)
       pass += str.charAt(char);
     }
