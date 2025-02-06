93ac3b5b8e00a68671e4007996a16942c3c4da84
Anirban Kapil <anirbankapil@gmail.com>
2025-02-06 11:24:28 +0530

    RTE API

--- megaBlog/src/components/RTE.jsx
+++ megaBlog/src/components/RTE.jsx
@@ -17,6 +17,7 @@ export default function RTE({name,control,label,defaultValue =""}) {
        control={control}
        render={({field : {onChange}}) => (
         <Editor 
+        apiKey= {import.meta.env.VITE_TINY_MCE_API}
         initialValue= {defaultValue}
         init={{
             initialValue : defaultValue,
