d32b717119bb32ed6b0cee425de7747d32d58c3b
Anirban Kapil <anirbankapil@gmail.com>
2025-02-12 23:45:08 +0530

    app logo

--- megaBlog/src/components/Logo.jsx
+++ megaBlog/src/components/Logo.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-function Logo({width = "100px"}) {
+
+function Logo() {
   return (
-    <div>Logo</div>
+    <div className='w-40 h-40'>
+      <img src='https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572-768x591.png'/>
+    </div>
   )
 }
 
